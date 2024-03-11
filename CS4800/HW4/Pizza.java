393dd6ef9c8c98139403b58b9399fc5c4cc13776
Marvin <=>
2024-03-11 10:46:47 -0700

    Redid HW4 Problem 1 I used a factory design pattern instead of a builder design pattern

--- CS4800/HW4/Pizza.java
+++ CS4800/HW4/Pizza.java
@@ -1,5 +1,77 @@
 package CS4800.HW4;
 
-public abstract class Pizza{
-    public abstract void eat();
+public class Pizza {
+    private String chainName; 
+    private String size; 
+    private String[] toppings;
+
+    private Pizza(PizzaBuilder builder)
+    {
+        this.size = builder.size;
+        this.toppings = builder.toppings;
+        this.chainName = builder.chainName;
+    }
+
+    public String getChainName()
+    {
+        return this.chainName;
+    }
+
+    public String getSize()
+    {
+        return this.size;
+    }
+
+    public String[] getToppings()
+    {
+        return this.toppings;
+    }
+
+    public void eat()
+    {
+        System.out.printf("\n\nChain Name: %s", this.getChainName());
+        System.out.printf("\nPizza Size: %s", this.getSize());
+        
+        int toppingCounter = 1;
+        for(String topping : this.getToppings())
+        {
+            System.out.printf("\nTopping #%d : %s", toppingCounter, topping);
+            toppingCounter++;
+        }
+    }
+
+    public static class PizzaBuilder{
+        private String chainName; 
+        private String size; 
+        private String[] toppings; 
+
+        public PizzaBuilder(String givenChainName)
+        {
+            this.chainName = givenChainName;
+        }
+
+        public PizzaBuilder setChainName(String givenChainName)
+        {
+            this.chainName = givenChainName;
+            return this;
+        }
+
+        public PizzaBuilder setSize(String givenSize)
+        {
+            this.size = givenSize;
+            return this;
+        }
+
+        public PizzaBuilder setToppings(String[] givenToppings)
+        {
+            this.toppings = givenToppings;
+            return this;
+        }
+
+        public Pizza makePizza()
+        {
+            return new Pizza(this);
+        }
+    }
+
 }
